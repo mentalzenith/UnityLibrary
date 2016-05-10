c315b1a3314769569f2eeed12a7618d841160fa7
Michael Yuen <mentalzenith@gmail.com>
2016-05-10 13:20:14 +0800

    Added SafeTween and ThreadManager

---
+++ SafeTween/SafeTweenManager.cs
@@ -0,0 +1,218 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using System;
+
+namespace SafeTween
+{
+    public class SafeTweenManager : MonoBehaviour
+    {
+        static SafeTweenManager _instance;
+
+        public static SafeTweenManager Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    CreateManager();
+                return _instance;
+            }
+        }
+
+        static Dictionary<object,Tweener> tweeners;
+        static List<Tweener> playing;
+        static List<Tweener> adding;
+        static List<Tweener> stopping;
+
+        static void CreateManager()
+        {
+            var go = new GameObject("[SafeTweenmanager]");
+            _instance = go.AddComponent<SafeTweenManager>();
+            Init();
+        }
+
+        static void Init()
+        {
+            tweeners = new Dictionary<object, Tweener>();
+            adding = new List<Tweener>();
+            playing = new List<Tweener>();
+            stopping = new List<Tweener>();
+        }
+
+        public void Add(object o, Tweener tweener)
+        {
+            //tweeners.Add(o, tweener);
+            Play(tweener);
+        }
+
+        public static void Stop(Tweener tweener)
+        {
+//            playing.Remove(tweener);
+            stopping.Add(tweener);
+        }
+
+        public static void Play(Tweener tweener)
+        {
+            adding.Add(tweener);
+        }
+
+        void Update()
+        {
+            playing.AddRange(adding);
+            adding.Clear();
+
+            foreach (var tweener in playing)
+                tweener.Update();
+
+            foreach (var tweener in stopping)
+                playing.Remove(tweener);
+            stopping.Clear();
+        }
+    }
+
+    public class Tweener
+    {
+        List<TweenPropertyBase> tweenProperties;
+        float time;
+
+        public Tweener()
+        {
+            tweenProperties = new List<TweenPropertyBase>();
+        }
+
+        public void Add(TweenPropertyBase tweenProperty)
+        {
+            tweenProperty.tweener = this;
+            tweenProperty.isPlaying = true;
+            tweenProperties.Add(tweenProperty);
+        }
+
+        public void Play()
+        {
+            time = 0;
+            SafeTweenManager.Play(this);
+        }
+
+        public void Stop()
+        {
+            SafeTweenManager.Stop(this);
+        }
+
+        public void Update()
+        {
+            time += Time.deltaTime;
+            foreach (var tweenProperty in tweenProperties)
+                if (tweenProperty.isPlaying)
+                    tweenProperty.Update(time);
+        }
+    }
+
+    public abstract class TweenPropertyBase
+    {
+        public Tweener tweener;
+        public float delay;
+        public float duration;
+        protected Action OnCompleteAction;
+
+        public bool isPlaying;
+
+        public abstract void Update(float time);
+
+        public virtual void OnComplete()
+        {
+            if (OnCompleteAction != null)
+                OnCompleteAction();
+            isPlaying = false;
+        }
+
+        public virtual TweenPropertyBase SetDelay(float delay)
+        {
+            this.delay = delay;
+            return this;
+        }
+
+        public virtual TweenPropertyBase SetCompleteAction(Action OnCompleteAction)
+        {
+            this.OnCompleteAction = OnCompleteAction;
+            return this;
+        }
+    }
+
+    public class TweenImageFill:TweenPropertyBase
+    {
+        Image image;
+        public float start;
+        public float end;
+
+        public TweenImageFill(Image image, float end, float duration)
+        {
+            this.image = image;
+            this.start = image.fillAmount;
+            this.end = end;
+            this.duration = duration;
+        }
+
+        public override void Update(float time)
+        {
+            if (time <= delay)
+                return;
+            
+            image.fillAmount = Mathf.Lerp(start, end, time - delay / duration);
+
+            if (time >= duration + delay)
+                OnComplete();
+        }
+    }
+
+    public class TweenPositionRelative:TweenPropertyBase
+    {
+        RectTransform rectTransform;
+        public Vector3 start;
+        public Vector3 endRelative;
+
+        public TweenPositionRelative(RectTransform rectTransform, Vector3 end, float duration)
+        {
+            this.rectTransform = rectTransform;
+            this.start = rectTransform.localPosition;
+            this.endRelative = end;
+            this.duration = duration;
+            this.OnCompleteAction = OnCompleteAction;
+        }
+
+        public override void Update(float time)
+        {
+            if (time <= delay)
+                return;
+
+            rectTransform.localPosition = Vector3.Lerp(start, start + endRelative, time - delay / duration);
+
+            if (time >= duration + delay)
+                OnComplete();
+        }
+    }
+
+    public static class ImageExtension
+    {
+        public static TweenImageFill Fill(this Image image, float target, float duration)
+        {
+            var tweener = new Tweener();
+            var tweenImageFill = new TweenImageFill(image, target, duration);
+            tweener.Add(tweenImageFill);
+            SafeTweenManager.Instance.Add(image, tweener);
+            return tweenImageFill;
+        }
+    }
+
+    public static class TransformExtension
+    {
+        public static TweenPositionRelative MovePositionRelative(this RectTransform rectTransform, Vector3 target, float duration)
+        {
+            var tweener = new Tweener();
+            var tweenPositionRelative = new TweenPositionRelative(rectTransform, target, duration);
+            tweener.Add(tweenPositionRelative);
+            SafeTweenManager.Instance.Add(rectTransform, tweener);
+            return tweenPositionRelative;
+        }
+    }
+}
---
+++ Threading/ThreadManager.cs
@@ -0,0 +1,151 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using System;
+using System.ComponentModel;
+
+public class ThreadManager : SingletonMono<ThreadManager>,ISynchronizeInvoke
+{
+	Queue<UnityAsyncResult> fifoToExecute = new Queue<UnityAsyncResult>();
+	Thread mainThread;
+	static bool initialized;
+
+	public static void Init()
+	{
+		if (initialized)
+			return;
+
+		var go = new GameObject("[Thread Manager]");
+
+		_instance = go.AddComponent < ThreadManager>();
+	}
+
+	void Awake()
+	{
+		mainThread = Thread.CurrentThread;
+	}
+
+	void Update()
+	{
+		ProcessQueue();
+	}
+
+	public bool InvokeRequired { get { return mainThread.ManagedThreadId != Thread.CurrentThread.ManagedThreadId; } }
+
+	public IAsyncResult BeginInvoke(Delegate method, object[] args)
+	{
+		var asyncResult = new UnityAsyncResult() {
+			method = method,
+			args = args,
+			IsCompleted = false,
+			AsyncWaitHandle = new ManualResetEvent(false),
+		};
+		lock (fifoToExecute)
+		{
+			fifoToExecute.Enqueue(asyncResult);
+		}
+		return asyncResult;
+	}
+
+	public IAsyncResult BeginInvoke(Action method, object[] args)
+	{
+		var asyncResult = new UnityAsyncResult() {
+			method = method,
+			args = args,
+			IsCompleted = false,
+			AsyncWaitHandle = new ManualResetEvent(false),
+		};
+		lock (fifoToExecute)
+		{
+			fifoToExecute.Enqueue(asyncResult);
+		}
+		return asyncResult;
+	}
+
+	public object EndInvoke(IAsyncResult result)
+	{
+		if (!result.IsCompleted)
+		{
+			result.AsyncWaitHandle.WaitOne();
+		}
+		return result.AsyncState;
+	}
+
+	public object Invoke(Delegate method, object[] args)
+	{
+		if (InvokeRequired)
+		{            
+			var asyncResult = BeginInvoke(method, args);
+			return EndInvoke(asyncResult);
+		}
+		else
+		{
+			return method.DynamicInvoke(args);
+		}
+	}
+
+	public object Invoke(Action method, object[] args)
+	{
+		if (InvokeRequired)
+		{            
+			var asyncResult = BeginInvoke(method, args);
+			return EndInvoke(asyncResult);
+		}
+		else
+		{
+			return method.DynamicInvoke(args);
+		}
+	}
+
+	void ProcessQueue()
+	{
+		if (Thread.CurrentThread != mainThread)
+		{
+			throw new Exception("Must be called from the same thread it was created on");
+		}
+		bool loop = true;
+		UnityAsyncResult data = null;
+		while (loop)
+		{
+			lock (fifoToExecute)
+			{
+				loop = fifoToExecute.Count > 0;
+				if (!loop)
+					break;
+				data = fifoToExecute.Dequeue();
+			}
+
+			data.AsyncState = Invoke(data.method, data.args);
+			data.IsCompleted = true;
+		}
+	}
+
+}
+
+public class UnityAsyncResult : IAsyncResult
+{
+	public Delegate method;
+	public object[] args;
+
+	public bool IsCompleted { get; set; }
+
+	public WaitHandle AsyncWaitHandle { get; internal set; }
+
+	public object AsyncState { get; set; }
+
+	public bool CompletedSynchronously { get { return IsCompleted; } }
+}
+
+public static class ISynchronizeInvokeExtension
+{
+	public static IAsyncResult BeginInvoke(this ISynchronizeInvoke synchronizeInvoke, Delegate method, object[] args)
+	{
+		return ThreadManager.Instance.BeginInvoke(method, args);
+	}
+
+	public static void Invoke(this ISynchronizeInvoke synchronizeInvoke, Action method, object[] args)
+	{
+		ThreadManager.Instance.Invoke(method, args);
+	}
+}
