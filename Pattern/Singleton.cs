dd256d07e6d54e01a9dbccd3814a1e1cfa1c3636
Michael Yuen <mentalzenith@gmail.com>
2016-05-10 13:18:37 +0800

    Added Webform and Singleton

---
+++ Pattern/Singleton.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SingletonMono<T> : MonoBehaviour where T : MonoBehaviour
+{
+	protected static T _instance;
+
+	public static T Instance {
+		get
+		{
+			if (_instance == null)
+				_instance = (T)FindObjectOfType(typeof(T));
+			
+			if (_instance == null)
+				Debug.LogError("Unity cannot find a type of " + typeof(T).Name + " in the scene");
+			
+			return _instance;
+		}
+	}
+
+	public static T InstanceDirect{ get { return _instance; } }
+}
+
+public class Singleton<T> where T:new()
+{
+	protected static T _instance;
+
+	public static T Instance {
+		get
+		{
+			if (_instance == null)
+				_instance = new T();
+
+			return _instance;
+		}
+	}
+}
