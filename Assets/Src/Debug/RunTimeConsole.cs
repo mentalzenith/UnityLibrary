e748e294da962a53871f4ffc775808e8f6731946
Michael Yuen <mentalzenith@gmail.com>
2016-05-20 15:10:46 +0800

    Made this into a Unity project

---
+++ Assets/Src/Debug/RunTimeConsole.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class RunTimeConsole : MonoBehaviour
+{
+	public Text logText;
+	
+	public void Run ()
+	{
+		Debug.Log ("Log something");
+	}
+	
+	public void Run2 ()
+	{
+		
+	}
+
+	void Start ()
+	{
+		Application.RegisterLogCallback (HandleLog);
+	}
+	
+	void HandleLog (string logString, string stackTrace, LogType type)
+	{
+		logText.text = logString + " \n" + stackTrace;
+	}
+}
