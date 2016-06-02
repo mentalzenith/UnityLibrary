6c2b6813d8dc85d4ec9570504e8476917f75d466
Michael Yuen <mentalzenith@gmail.com>
2016-06-02 14:58:23 +0800

    Updated FPS Chart

---
+++ Assets/Debug/FPSChart/FPSChart.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FPSChart : MonoBehaviour
+{
+    public int dataSampleSize = 100;
+    public Vector2 size = new Vector2(200, 100);
+    public float maxTime = 0.1f;
+
+    Texture2D data;
+    Color[] colors;
+    float[] deltaTimes;
+    float averageDeltaTime;
+    float maxDeltaTime;
+
+    int _index;
+
+    int Index
+    {
+        get
+        {
+            return _index;
+        }
+        set
+        {
+            if (value < data.width)
+                _index = value;
+            else
+                _index = 0;
+        }
+    }
+
+    Material material;
+
+    void Awake()
+    {
+//        data = new Texture2D(dataSampleSize, 10);
+        data = new Texture2D(dataSampleSize, 1);
+        deltaTimes = new float[dataSampleSize];
+        colors = new Color[dataSampleSize];
+        var shader = Shader.Find("Debug/FPSChart");
+        if (shader != null)
+            material = new Material(shader);
+        else
+            Debug.Log("Shader not found");
+    }
+
+    void Update()
+    {
+        deltaTimes[Index++] = Time.deltaTime;
+//        ReScale();
+//        data.SetPixels(colors);
+        maxDeltaTime = GetMaxDeltaTime();
+        averageDeltaTime = GetAverage();
+        var normalizedValue = Time.deltaTime / maxTime;
+        data.SetPixel(Index, 0, new Color(normalizedValue, 0, 0));
+        data.Apply();
+    }
+
+    void ReScale()
+    {
+        maxDeltaTime = GetMaxDeltaTime();
+        averageDeltaTime = GetAverage();
+        for (int i = 0; i < data.width; i++)
+            colors[i] = new Color((deltaTimes[i] - averageDeltaTime) / maxDeltaTime, 0, 0);
+    }
+
+    float GetMaxDeltaTime()
+    {
+        float max = 0;
+        for (int i = 0; i < deltaTimes.Length; i++)
+            if (deltaTimes[i] > max)
+                max = deltaTimes[i];
+        return max;
+    }
+
+    float GetAverage()
+    {
+        float avg = 0;
+        for (int i = 0; i < deltaTimes.Length; i++)
+            avg += deltaTimes[i];
+        return avg / deltaTimes.Length;
+    }
+
+    void OnGUI()
+    {
+        if (Event.current.type.Equals(EventType.Repaint))
+            Graphics.DrawTexture(new Rect(0, Screen.height - size.y, size.x, size.y), data, material);
+
+        var fontStyle = new GUIStyle();
+        fontStyle.fontSize = (int)size.y / 3;
+        fontStyle.normal.textColor = Color.green;
+        GUI.Label(new Rect(size.x, Screen.height - size.y, 100, size.y / 3), "Low: " + (1 / maxDeltaTime).ToString(), fontStyle);
+        GUI.Label(new Rect(size.x, Screen.height - size.y / 3, 100, size.y / 3), "Avg: " + (1 / averageDeltaTime).ToString(), fontStyle);
+    }
+}
---
+++ Assets/Debug/FPSChart/FPSChart.shader
@@ -0,0 +1,61 @@
+﻿Shader "Debug/FPSChart"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+	}
+	SubShader
+	{
+		Tags { "RenderType" = "Transparent" "Queue" = "Transparent" }
+		LOD 100
+
+		Pass
+		{
+		 	Blend SrcAlpha OneMinusSrcAlpha
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			// make fog work
+			#pragma multi_compile_fog
+			
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				UNITY_FOG_COORDS(1)
+				float4 vertex : SV_POSITION;
+			};
+
+			sampler2D _MainTex;
+			float4 _MainTex_ST;
+			
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.vertex = mul(UNITY_MATRIX_MVP, v.vertex);
+				o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+				UNITY_TRANSFER_FOG(o,o.vertex);
+				return o;
+			}
+			
+			fixed4 frag (v2f i) : SV_Target
+			{
+				// sample the texture
+				fixed4 data = tex2D(_MainTex, i.uv);
+				fixed4 col= fixed4(0,0,0,0);
+				if(i.uv.y<data.r)
+					col = fixed4(i.uv.y,1-i.uv.y,0,1);
+
+				return col;
+			}
+			ENDCG
+		}
+	}
+}
