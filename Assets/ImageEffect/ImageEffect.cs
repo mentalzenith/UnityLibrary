ba50362a74a73a51c4dbd91d77eb024ebd6f76cc
Michael Yuen <mentalzenith@gmail.com>
2016-06-07 10:15:08 +0800

    Added Flip ImageEffect

---
+++ Assets/ImageEffect/ImageEffect.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ImageEffect : MonoBehaviour
+{
+    public Shader shader;
+    public RenderTexture renderTexture;
+    Material material;
+
+    void Awake()
+    {
+        material = new Material(shader);
+        if (renderTexture == null)
+            renderTexture = new RenderTexture(Screen.width, Screen.height, 24);
+    }
+
+    public void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, renderTexture, material);
+        Graphics.Blit(source, destination);
+    }
+}
